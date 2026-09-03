02dd989232fd5269d33fcaf13680f2c8f0fea436
Anthony-D-Martin <anthonydavmartin@gmail.com>
2026-09-03 19:36:38 +0000

    update

--- 000_Labs/003_output/base_code/starter.java
+++ 000_Labs/003_output/base_code/starter.java
@@ -1,14 +1,17 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author: Anthony Martin 
+ *  Date: 9/2/26
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		//the string "I love to learn coding remotely." will appear in
+		//the command window when you compile and run this program.
+		System.out.println("Happy Birthday, to you.");
+		System.out.println("Happy Birthday, to you.");
+		System.out.println("Happy Birthday, dear Bob.");
+		System.out.println("Happy Birthdat, to you.");
 	}
 }
--- 000_Labs/004_String_class/base_code/starter.java
+++ 000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String HB=new String("Happy Birthday ");
+		String TY=new String("to you.");
+		String DR=new String("dear proper name of somebody in the class,");
+		System.out.println(HB+TY);
+		System.out.println(HB+TY);
+		System.out.println(HB+DR);
+		System.out.println(HB+TY);
 	}
 }
