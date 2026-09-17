3e30d4f37dfb56416a89faae81b3fdfe4530a7d2
Anthony-D-Martin <anthonydavmartin@gmail.com>
2026-09-17 18:53:02 +0000

    update

--- 100_HW/HW3_Divisible/base_code/starter.java
+++ 100_HW/HW3_Divisible/base_code/starter.java
@@ -1,7 +1,7 @@
 /*
- *	Author:
- *  Date:
- * 	Collaborator: 
+ *	Author: Anthony Martin
+ *  Date: 12/25
+ * 	Collaborator: Flowery
 */
 
 import java.util.Scanner;
@@ -10,6 +10,18 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely.");
+
+//	- Create a program that takes in two input integers. 
+//- First check if each of these integers are odd or even. Output if each are or not.
+//- Then check if they're divisible by 3, 4, and 5. Make sure ALL of these are checked.
+//- Otherwise if it's not divisible by all 3, 4, and 5, output that it isn't! Only if all 3!
+
+
+
+		
+		System.out.println(Math.random()*100);
+
+		
+		
 	}
 }
