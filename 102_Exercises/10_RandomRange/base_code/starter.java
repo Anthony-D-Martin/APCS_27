3a946bbc470b6a0d74f030f6bc57173944896c90
Anthony-D-Martin <anthonydavmartin@gmail.com>
2026-09-21 19:55:56 +0000

    update

--- 102_Exercises/10_RandomRange/base_code/starter.java
+++ 102_Exercises/10_RandomRange/base_code/starter.java
@@ -1,15 +1,30 @@
 /*
- *	Author:
- *  Date:
- *	Collaborator(s): 
+ *	Author: Anthony Martin
+ *  Date: 9/17/2026
+ *	Collaborator(s): Triple T
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		// I made it inclusive
+		System.out.println("Enter 2 numbers to create a range for your random number");
+		System.out.println("Please enter an integer:"); 
+		Scanner sc=new Scanner(System.in);
+		int num1=sc.nextInt();
+		sc.nextLine();
+		System.out.println("Please enter another integer (bigger than the first):");
+		int num2=sc.nextInt();
+		sc.nextLine();
+		System.out.println("");
+		System.out.println("Your range is "+num1+"to "+num2+".");
+		System.out.println("Here are 5 numbers generated in that range.");
+		int out1=(int)((Math.random()*((num2+1)-num1))+num1);
+		int out2=(int)((Math.random()*((num2+1)-num1))+num1);
+		int out3=(int)((Math.random()*((num2+1)-num1))+num1);
+		int out4=(int)((Math.random()*((num2+1)-num1))+num1);
+		int out5=(int)((Math.random()*((num2+1)-num1))+num1);
+		System.out.println(out1+","+out2+","+out3+","+out4+","+out5);
 	}
 }
