c4ff2b4adf2a3150729b6a8da7f0e22b267e79d7
Anthony-D-Martin <anthonydavmartin@gmail.com>
2026-09-24 18:55:16 +0000

    update: the sequel

--- 000_Labs/011_if_statements/base_code/starter.java
+++ 000_Labs/011_if_statements/base_code/starter.java
@@ -9,6 +9,18 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
-	}
+	
+		Scanner sc=new Scanner(System.in);
+		System.out.println("What is the first integer?");
+		int x=sc.nextInt();
+		System.out.println("What is the second integer?");
+		int y=sc.nextInt();
+		boolean same=x==y;
+		boolean diff=x!=y;
+		if(same){
+		System.out.println(x+" is the same as "+y+"!");
+		}
+		if(diff){
+		System.out.println(x+" is not the same as "+y+"!");	
+		}
 }
